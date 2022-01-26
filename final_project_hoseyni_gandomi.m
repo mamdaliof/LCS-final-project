0aab8b66ad2cba3cf2c2b94500f241abf063565f
Mamdaliof <mohammadhosini60@gmail.com>
2022-01-26 21:51:16 +0330

    complte part B

--- final_project_hoseyni_gandomi.m
+++ final_project_hoseyni_gandomi.m
@@ -21,5 +21,26 @@ xlabel('frequency(rad/s)')
 %---------------------part A 2-------------------%
 approximateSys=0.3981*(-s/5+1)/(s/5+1)/s/(s^2/25+2/5*0.09752*s+1);
 zpk(approximateSys)
-figure
-bode(approximateSys)
+figure('name','approximate System')
+margin(approximateSys)
+%---------------------part B 1-------------------%
+figure('name','pzmap')
+pzmap(approximateSys)
+figure('name','root locus')
+rlocus(approximateSys)
+hold on
+rlocus(-approximateSys,'--')
+%---------------------part B 2-------------------%
+figure('name','responses')
+subplot(1,2,1)
+k=1.5;
+step(feedback(k*approximateSys,1))
+title('step response')
+subplot(1,2,2)
+step(feedback(k*approximateSys,1)/s)
+title('ramp response')
+hold on 
+h=ezplot('x',[0,20]);
+set(h, 'Color', 'r');
+legend('ramp response','y=x')
+%---------------------part C 1-------------------%
