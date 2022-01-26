d0ac5442018785e0c173f1756f804acb9028b568
Mamdaliof <mohammadhosini60@gmail.com>
2022-01-26 20:43:27 +0330

    extract data and plot bode

--- final_project_hoseyni_gandomi.m
+++ final_project_hoseyni_gandomi.m
@@ -1,1 +1,13 @@
-xlsread(
+clc
+clear
+fileName='Data.xlsx';
+Data=xlsread(fileName);
+time=Data(:,1);
+phase=Data(:,3);
+magnitude=Data(:,2);
+magnitudeConverted=mag2db(magnitude);
+figure('name','bode diagram')
+subplot(2,1,1)
+semilogx(time,magnitudeConverted);
+subplot(2,1,2)
+semilogx(time,phase);
